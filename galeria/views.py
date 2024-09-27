8a76c61b4fd883bbbab9b2abf1b9339128178170
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-09-27 16:42:46 -0300

    feat : new dates into database and filter dates on admin

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,7 @@ from galeria.models import Fotografia
 
 def index(request):
 
-    fotografia = Fotografia.objects.all()
+    fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     return render(request,'galeria/index.html', {"cards" : fotografia })
 
