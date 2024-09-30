56b4523474ad6a615c206fa907fdbe67dcfbf34e
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-09-30 14:16:40 -0300

    feat: update url images and update route

--- setup/settings.py
+++ setup/settings.py
@@ -127,6 +127,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Arquivos de media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
