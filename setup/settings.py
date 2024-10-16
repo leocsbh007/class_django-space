82236d2cb06fd2ffe72c27accc5d6acf443cf8bf
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-10-16 17:35:12 -0300

    feat: Form Class to controller login and usuario

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
-    'usuarios.apps.UsuaruosConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
