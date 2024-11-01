9125cbfb0989b79f8f29e243383d8a8ce7b6571b
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-11-01 14:44:23 -0300

    feat: Create user into cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForms()
@@ -8,4 +9,33 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+    
+    # Valida se o formulario é valido
+    if form.is_valid():
+        # Verifica as senhas digitadas são diferentes
+        if form['senha'].value() != form['confirmar_senha'].value():
+            return redirect('cadastro')
+        
+        # Recupera os campos do formulario
+        nome = form['nome_cadastro'].value()
+        email = form['email'].value()
+        senha = form['senha'].value()
+
+        # Verifica dentro da tabela interna do Django 
+        # se um usuario existe
+        if User.objects.filter(username=nome).exists():
+            return redirect('cadastro')
+        
+        # Cria um usuario e salva dentro do banco interno do Django
+        usuario = User.objects.create_user(
+            username=nome,
+            email=email,
+            password=senha
+        )
+
+        usuario.save()
+        return redirect('login')        
+
     return render(request, 'usuarios/cadastro.html', {'form' : form})
