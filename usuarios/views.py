97820d69d7d0f051b6d5f40eb9ff5779da1b531e
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-11-01 16:12:01 -0300

    feat: Auth login and routes success and failure

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def login(request):
     form = LoginForms()
+
+    # Garante que o metodo de entrda é do tipo POST
+    if request.method == 'POST':
+        # Pega o formulario com os campos enviados no POST
+        form = LoginForms(request.POST)
+        # Valida o formulario
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+        # Cria um usuario com os dados vindos do formulario
+        # e valida que pode ser feito o login
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        # Verifica se os dados não estão vazios
+        if usuario is not None:
+            # Faz a autenticação
+            auth.login(request, usuario)
+            return redirect ('index')
+        else:
+            return redirect('login')
+
     return render(request, 'usuarios/login.html', {'form' : form})
 
 
